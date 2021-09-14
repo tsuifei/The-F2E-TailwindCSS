2a2aa349a1e2ef472f316e3a625cb3b1d0bb1305
tsuifei <tsuifei@gmail.com>
2021-09-14 13:05:56 +0200

    [deploy] - deployed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: {content: ['./**/*.html', './**/*.js']},
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  mode: 'jit'
+}
